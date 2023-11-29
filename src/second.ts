fd6d9af148f0ca2d6789d490f73577b6213680fd
Narsis <narsiskh.2013@gmail.com>
2023-11-29 13:29:43 +0330

    The importance of interface.

--- src/second.ts
+++ src/second.ts
@@ -4,8 +4,27 @@ interface TakePhoto {
     burst: number
 }
 
+interface Story {
+    createStory(): void
+}
+
 class Instagram implements TakePhoto {
-    constructor() {
+    constructor(
         public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ){}
+}
+
+class Youtube implements TakePhoto, Story{
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number,
+        public short: string
+    ){}
+
+    createStory(): void{
+        console.log("Story was created.")
     }
 }
