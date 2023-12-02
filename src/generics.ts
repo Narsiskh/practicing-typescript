c2903af3d8489aa218efce6a439528486a921352
Narsis <narsiskh.2013@gmail.com>
2023-12-02 14:54:16 +0330

    Learning about  Generics in Array and Arrow functions

--- src/generics.ts
+++ src/generics.ts
@@ -21,4 +21,15 @@ interface Bootle {
     type: number
 }
 
-identityFour<Bootle>({brand: "meta", type: 222})
+// identityFour<Bootle>({})
+
+function getSearchProducts<T>(products: T[]): T {
+    // do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+const getMoreSearchProducts = <T,>(products: T[]): T => {
+    // do some database operations
+    const myIndex = 4
+    return products[myIndex]
+}
